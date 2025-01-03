88df8df81ad9fcab08dd606bb28288867e9fbdb9
916-22k <safenth2@gmail.com>
2025-01-03 03:51:25 +0000

    Mobile Swiping

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 let square = document.getElementById('blue-square');
-let position = window.innerWidth / 2;
+let positionX = window.innerWidth / 2;
 let startX = 0;
+let startY = 0;
 
 function updatePosition(delta) {
-    position += delta * 0.1;
-    square.style.left = position + 'px';
+    positionX += delta * 0.1;
+    square.style.left = positionX + 'px';
 }
 
 window.addEventListener('wheel', (event) => {
-    updatePosition(event.deltaY);
+    // Combine horizontal (deltaX) and vertical (deltaY) scroll into horizontal movement
+    updatePosition(event.deltaX + event.deltaY);
 });
 
 window.addEventListener('touchstart', (event) => {
     startX = event.touches[0].clientX;
+    startY = event.touches[0].clientY;
 });
 
 window.addEventListener('touchmove', (event) => {
     let deltaX = event.touches[0].clientX - startX;
-    updatePosition(-deltaX);
+    let deltaY = event.touches[0].clientY - startY;
+
+    // Treat vertical movement (deltaY) as horizontal movement
+    updatePosition(deltaX + deltaY);
+
     startX = event.touches[0].clientX;
+    startY = event.touches[0].clientY;
 });
