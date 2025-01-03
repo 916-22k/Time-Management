39fb309075fb702e250f4c068fa321dd5ce6307b
916-22k <safenth2@gmail.com>
2025-01-03 03:33:25 +0000

    Mobile swiping

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 let square = document.getElementById('blue-square');
 let position = window.innerWidth / 2;
+let startX = 0;
+
+function updatePosition(delta) {
+    position += delta * 0.1;
+    square.style.left = position + 'px';
+}
 
 window.addEventListener('wheel', (event) => {
-    position += event.deltaY * 0.1;
-    square.style.left = position + 'px';
+    updatePosition(event.deltaY);
+});
+
+window.addEventListener('touchstart', (event) => {
+    startX = event.touches[0].clientX;
+});
+
+window.addEventListener('touchmove', (event) => {
+    let deltaX = event.touches[0].clientX - startX;
+    updatePosition(-deltaX);
+    startX = event.touches[0].clientX;
 });
